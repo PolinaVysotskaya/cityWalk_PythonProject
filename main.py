30f926dc119d57f99066adf7d420a03bbbcf5ebb
polisha <pvysotskaya564@gmail.com>
2024-02-12 16:41:05 +0300

    new

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
-from kivymd.app import MDApp
+from kivy.app import App
 from kivymd.uix.label import MDLabel
+from kivy.lang import Builder
+from kivymd.uix.card import MDCard
+from kivy.uix.screenmanager import Screen
 
 
-class CityWalkApp(MDApp):
+class HomeScreen(Screen):
+    pass
+
+
+class CardsScreen(Screen):
+    pass
+
+
+GUI = Builder.load_file("main.kv")
+
+
+class CityWalkApp(App):
     def build(self):
-        return MDLabel(text="City Walk", halign="center")
+        return GUI
+
+    def change_screen(self, screen_name):
+        screen_manager = self.root.ids['screen_manager']
+        # screen_manager.transition
+        screen_manager.current = screen_name
 
 
 CityWalkApp().run()
